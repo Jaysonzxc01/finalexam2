eb37a195d4da504166d9ce17ead66b9aa5a16adb
jaysonzxc01 <jaysonquiachon71@gmail.com>
2024-06-04 12:09:06 -0800

    latest

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -30,5 +30,21 @@
             <!-- /.row -->
         </div><!-- /.container-fluid -->
     </div>
+    <div class="row" style="margin:30px;">
+        <div class="col-lg-3 col-6">
+          <!-- small box -->
+          <div class="small-box bg-info">
+            <div class="inner">
+              <h3>1250</h3>
+
+              <p>Employees</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <a href="{{ route('users.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
     <!-- /.content -->
 @endsection
